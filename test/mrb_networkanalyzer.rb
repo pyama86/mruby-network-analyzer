275a032c8d346ef9a1df222601081007f2bd3e23
pyama86 <pyama@pepabo.com>
2017-06-16 02:18:23 +0900

    fix ci

--- test/mrb_networkanalyzer.rb
+++ test/mrb_networkanalyzer.rb
@@ -5,7 +5,7 @@
 assert("NetworkAnalyzer#new") do
   n = NetworkAnalyzer.new("lo")
   system('ping 127.0.0.1 -c 1')
-  assert_true(n.current.is_a?(Array))
+  assert_true(n.current.first['src_host'] == '127.0.0.1')
   n.stop
 end
 
@@ -14,5 +14,5 @@ assert("NetworkAnalyzer#stop") do
   system('ping 127.0.0.1 -c 1')
   n.stop
   system('ping 127.0.0.1 -c 1')
-  assert_true(n.current.is_a?(Array))
+  assert_true(n.current.first['src_host'] == '127.0.0.1')
 end
