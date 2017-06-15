52a5d1f3b84b3847fd676a96a3b912ee929d7feb
pyama86 <pyama@pepabo.com>
2017-06-16 02:06:20 +0900

    signal task to mruby-signal-thread

--- mrblib/mrb_networkanalyzer.rb
+++ mrblib/mrb_networkanalyzer.rb
@@ -1,5 +1,6 @@
 class NetworkAnalyzer
   def initialize(name)
+    ::SignalThread.mask(:INT)
     self._new(name)
     pr = Proc.new do
       self._collect(name)
--- src/mrb_networkanalyzer.c
+++ src/mrb_networkanalyzer.c
@@ -71,12 +71,6 @@ static const struct mrb_data_type mrb_networkanalyzer_data_type = {
     "mrb_networkanalyzer_data", mrb_free,
 };
 
-sig_atomic_t foad;
-
-static void finish(int sig) {
-    foad = sig;
-}
-
 int get_addrs_ioctl(mrb_state *mrb, mrb_value *self, char *interface)
 {
   int s;
@@ -440,9 +434,6 @@ static mrb_value mrb_networkanalyzer_new(mrb_state *mrb, mrb_value self)
   mrb_networkanalyzer_data *data;
   char *if_name;
   int if_name_len;
-  struct sigaction sa = {};
-  sa.sa_handler = finish;
-  sigaction(SIGINT, &sa, NULL);
 
   mrb_get_args(mrb, "s", &if_name, &if_name_len);
 
