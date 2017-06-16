424c9fb68a5446eaa09bc9d5cc08f549ee8d14c6
pyama86 <pyama@pepabo.com>
2017-06-16 15:46:57 +0900

    debug

--- test/mrb_networkanalyzer.rb
+++ test/mrb_networkanalyzer.rb
@@ -8,11 +8,11 @@ assert("NetworkAnalyzer#new") do
   assert_true(n.current.first['src_host'] == '127.0.0.1')
   n.stop
 end
-#
-#assert("NetworkAnalyzer#stop") do
-#  n = NetworkAnalyzer.new("lo")
-#  system('ping 127.0.0.1 -c 3')
-#  n.stop
-#  system('ping 127.0.0.1 -c 3')
+
+assert("NetworkAnalyzer#stop") do
+  n = NetworkAnalyzer.new("lo")
+  system('ping 127.0.0.1 -c 3')
+  n.stop
+  system('ping 127.0.0.1 -c 3')
 #  assert_true(n.current.first['src_host'] == '127.0.0.1')
-#end
+end
