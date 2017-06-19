6358bdaff100bfdf087f0ed706a263151c4f377b
pyama86 <pyama@pepabo.com>
2017-06-19 19:32:10 +0900

    unused thread and change interface

--- example/example.rb
+++ example/example.rb
@@ -1,3 +1,1 @@
-n = NetworkAnalyzer.new("lo")
-system('ping 127.0.0.1 -c 3')
-puts n.current.first
+puts NetworkAnalyzer.collect("lo", 5)
--- test/mrb_networkanalyzer.rb
+++ test/mrb_networkanalyzer.rb
@@ -2,9 +2,7 @@
 ## NetworkAnalyzer Test
 ##
 
-assert("NetworkAnalyzer#new") do
-  n = NetworkAnalyzer.new("lo")
-  system('ping 127.0.0.1 -c 3')
-  assert_true(n.current.first['src_host'] == '127.0.0.1')
-  n.stop
+assert("NetworkAnalyzer#collect") do
+  system('ping 127.0.0.1 -c 3 &')
+  assert_true(NetworkAnalyzer.collect("lo", 3).first['src_host'] == '127.0.0.1')
 end
